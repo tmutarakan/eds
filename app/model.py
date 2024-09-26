3a426ff80e04427d858625f1c7059d659071c99d
tmutarakan <ratibor.tmutarakansky@yandex.ru>
2024-09-26 19:26:08 +0300

    init project

--- app/model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-
-class Creature(BaseModel):
-    name: str
-    country: str
-    area: str
-    description: str
-    aka: str
---
+++ src/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def top():
+    return "top here"
+
+
+@app.get("/echo/{thing}")
+def echo(thing):
+    return f"echoing {thing}"
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
